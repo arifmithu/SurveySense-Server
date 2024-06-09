ac73c455cd64dd17c4568913a8d00e1b88ab56c8
arifmithu <mdarifhossainmithu@gmail.com>
2024-06-09 15:32:00 +0600

    added get a specific user api

--- index.js
+++ index.js
@@ -216,6 +216,13 @@ async function run() {
         res.send(result);
       }
     });
+    // get specific user to update status
+    app.get("/users/targetUser/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollections.findOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
